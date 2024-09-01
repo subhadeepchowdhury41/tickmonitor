4e47b72eedbdcfd27e639fc3d517ed9041e001a5
Subhadeep Chowdhury <subhadepchowdhury41@gmail.com>
2024-09-01 10:49:41 +0530

    splash

--- tick-monitor-web/src/app/page.tsx
+++ tick-monitor-web/src/app/page.tsx
@@ -1,29 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
-import Button from "@/components/ui/form/Button";
+import TickMGif from "@/assets/gifs/tickmonitor.gif";
 import axios from "axios";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {}, []);
+  const router = useRouter();
+  const checkMe = async () => {
+    await axios
+      .get("/api/auth/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.response.user) {
+          router.push("/dashboard/task");
+        } else {
+          router.push("/login");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        router.replace("/login");
+      });
+  };
+  useEffect(() => {
+    checkMe();
+  }, []);
   return (
-    <div className="w-full h-screen grid grid-cols-2">
-      <div>
-        <Button
-          label="Send"
-          onClick={async () => {
-            await axios
-              .get("/api/auth/me", {
-                withCredentials: true,
-              })
-              .then((res) => {
-                console.log(res.data);
-              })
-              .catch((err) => console.error(err));
-          }}
-        />
+    <div className="w-full h-screen flex items-center justify-center bg-slate-900">
+      <div className="m-4 rounded-lg bg-slate-900 flex items-center justify-center">
+        <Image src={TickMGif} alt="tickm-gif" />
       </div>
-      <div></div>
     </div>
   );
 }
